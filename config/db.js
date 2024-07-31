32bdc72068b94afffbccd3c28face4697a0c0949
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-31 08:40:46 +0530

    add devcamper master

--- config/db.js
+++ config/db.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-   
+    try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
-            useNewUrlIndex: true,
-            useFindAndModify:false,
+            useCreateIndex: true,
+            useFindAndModify: false,
             useUnifiedTopology: true // This option is recommended
         });
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
- 
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1); // Exit process with failure
+    }
 };
 
 module.exports = connectDB;

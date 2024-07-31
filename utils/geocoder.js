1b21985b37e6f250866ae485d51999c8b9bc63e3
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-31 10:50:53 +0530

    add devcamper master

--- utils/geocoder.js
+++ utils/geocoder.js
@@ -1,10 +1,30 @@
-const nodeGeocoder = require('node-geocoder');
+const axios = require('axios');
+const ErrorResponse = require('../utils/ErrorResponse'); // Corrected path
 
-const options ={
-    provider: process.env.GEOCODER_PROVIDER,
-    httpAdapter:'https',
-    apiKey: process.env.GEOCODER_API_KEY,
-    formatter:null
-}
-const geocoder = nodeGeocoder(options);
-module.exports = geocoder;
+const geocode = async (address) => {
+    try {
+        const response = await axios.get('https://www.mapquestapi.com/geocoding/v1/address', {
+            params: {
+                key: process.env.MAPQUEST_API_KEY, // Ensure this is set in your environment variables
+                location: address
+            }
+        });
+
+        if (response.data.info.statuscode !== 0) {
+            throw new ErrorResponse('Error fetching geocode data from MapQuest', 500);
+        }
+
+        const location = response.data.results[0].locations[0];
+        return {
+            latitude: location.latLng.lat,
+            longitude: location.latLng.lng,
+            formattedAddress: `${location.street}, ${location.adminArea5}, ${location.adminArea3}, ${location.postalCode}, ${location.adminArea1}`
+        };
+    } catch (error) {
+        throw new ErrorResponse(error.message, error.response ? error.response.status : 500);
+    }
+};
+
+module.exports = {
+    geocode
+};

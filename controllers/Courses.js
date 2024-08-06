7edf4a65554057eaa88368212a8f42685db46638
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-06 14:31:45 +0530

    add devcamper master

--- controllers/Courses.js
+++ controllers/Courses.js
@@ -6,7 +6,17 @@ const Course = require('../models/Course');
 // @route   GET /api/v1/bootcamps/:bootcampId/courses
 // @access  Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
-  
+  const course = await Course.findById(req.params.id).populate({
+    path: 'bootcamp',
+    select: 'name decription'
+  });
+
+  if(!course) {
+    return next(
+        new ErrorResponse('no course with the id of ${req.params.id'),
+    404
+);
+  }
     res.status(200).json({
         success: true,
         count: courses.length,

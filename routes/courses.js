5d87fdc88c8368f5b683b8fed650e3abd695754c
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-06 15:48:23 +0530

    add devcamper master

--- routes/courses.js
+++ routes/courses.js
@@ -2,15 +2,14 @@ const express = require('express');
 const {
     getCourses,
     getCourse,
-    addCourse
+    addCourse,
+    updateCourse,
+    deleteCourse
 } = require('../controllers/courses');
 
-const router = express.Router({mergeParams: true});
+const router = express.Router({ mergeParams: true });  // This is important to allow nested routes
 
-
-router.route('/:id')
-.get(getCourse)
-.post(addCourse);
-router.route('/:id').get(getCourses);
+router.route('/').get(getCourses).post(addCourse);
+router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;

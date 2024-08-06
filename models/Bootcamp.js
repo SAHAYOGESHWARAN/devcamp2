ffae4aec54a21dda808116155f30f0e126acab40
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-06 11:30:58 +0530

    add devcamper master

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -135,7 +135,8 @@ BootcampSchema.pre('save', async function(next) {
 });
 
 // cascade delete course when a bootcamp is deleted 
-BootcampSchema.pre('remove', async function(next) {
+BootcampSchema.pre('remove', async function(next) { 
+    console.log('Courses begin removed from bootcamp ${this._id}');
     await this.model('Course').deleteMany({bootcamp: this._id});
     next();
 });
